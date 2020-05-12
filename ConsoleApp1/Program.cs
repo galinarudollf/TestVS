a31c82def1fbf6f3168b6a84470b2b92a2a1474d
galinarudollf <49473764+galinarudollf@users.noreply.github.com>
2020-05-12 16:26:40 +0200

    eine Zeile hinzu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo, alle!");
+            Console.WriteLine("Noch eine Ausgabe");
         }
     }
 }
